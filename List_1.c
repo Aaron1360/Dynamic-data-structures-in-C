5003b8e27417cc81c35902f6ee253b13ac503269
Aaron Moreno Villeda <aaronmvilleda@gmail.com>
2022-10-07 08:57:00 -0500

    Update List_1.c

--- List_1.c
+++ List_1.c
@@ -19,7 +19,6 @@ struct LIST *getNewItem(const char product[20],int account)
         newItem->next = NULL;
         strcpy(newItem->product,product);
         newItem->account=account;
-        printf("Create new item at %p\n\n",newItem);
     }
 
     else
@@ -38,29 +37,96 @@ void printData(const struct LIST *product,const char *comment)
     }
     else
     {
-        printf("%s Article: %s x %d\n\n",comment,product->product,product->account);
+        printf("%s Article: %s x %d in %p pointing to %p\n\n",comment,
+        product->product,product->account,product,product->next);
     }
 }
 
+void printList(const struct LIST *list)
+{
+    const struct LIST *temp;
+    temp=list;
+    
+    if(temp==NULL)
+    {
+        printf("\nLIST IS EMPTY\n");
+    }
+    else
+    {
+        while(temp)
+        {
+            printData(temp,"t");
+            temp=temp->next;
+        }
+    }
+}
+
+void clean(struct LIST *list)
+{
+    struct LIST *next;
+    while(list)
+    {
+        next=list->next;
+        printf("Cleaning %s\n", list->product);
+        free(list);
+        list = next;
+    }
+}
 int main()
 {
+    printf("\n** START **\n\n");
+    
+    char command[64];
     char product[20];
     int account;
 
-    struct LIST *first = malloc(sizeof(struct LIST));
+    struct LIST *first = NULL;
     struct LIST *added = NULL;
-
-    printf(">>>LINKED LIST 1<<<<<\n\n");
-    printf("Enter a new product: ");
-    scanf("%s",&product);
-    printf("Enter # of items: ");
-    scanf("%d",&account);
     
-    first = getNewItem(product,account);
+    while(1)
+    {
+        printf( "Enter a command or value : ");
+		fgets(command, 64, stdin);
+		
+		if (strcmp("q\n", command) == 0) 
+		{
+			printf("Quitting..\n");
+			break;
+		}
+		
+		else if (strcmp("print\n", command) == 0) 
+        {
+            printf("Printing..\n");
+            printList(first);
+        }
+        
+        else if(sscanf(command, "%s %d", product,&account) != 0)
+        {
+            printf("Adding %s\n", product);
+            
+            if (first == NULL)
+            {
+                first = getNewItem(product,account);
+                if(first != NULL)
+                {
+                    added = first;
+                }
+            }
+            
+            else
+            {
+                added->next = getNewItem(product,account);
+                if(added->next != NULL)
+                {
+                    added = added->next;
+                }
+            }
+        }
+    }
     
-    printf("\n<<<FIRST<<<<\n");
-    printData(first,"first");
-
+    clean(first);
+    first = NULL;
+    added = NULL;
+    
     return 0;
 }
-
