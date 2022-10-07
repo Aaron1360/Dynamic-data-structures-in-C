494c85d951f9541a1b6120bd70ca439e29b9f7f8
Aaron Moreno Villeda <hh1360@hotmail.com>
2022-10-06 20:45:14 -0500

    first item added in List_1

--- List_1.c
+++ List_1.c
@@ -2,18 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct List
+struct LIST
 {
     char product[20];
     int account;
-    struct List *nextProduct; 
+    struct LIST *next;
 };
 
-struct List *newProduct(const char product[20],const int account);
-void printList(const struct List *pList,const char *comment);
+struct LIST *getNewItem(const char product[20],int account)
+{
+    struct LIST *newItem = NULL;
+    newItem = malloc(sizeof(struct LIST));
+
+    if(newItem != NULL)
+    {
+        newItem->next = NULL;
+        strcpy(newItem->product,product);
+        newItem->account=account;
+        printf("Create new item at %p\n\n",newItem);
+    }
+
+    else
+    {
+        printf("¡¡¡¡ALOCATION FAILURE!!!\n");
+    }
+
+    return newItem;
+}
+
+void printData(const struct LIST *product,const char *comment)
+{
+    if(product == NULL)
+    {
+        printf("%s is NULL\n\n",comment);
+    }
+    else
+    {
+        printf("%s Article: %s x %d\n\n",comment,product->product,product->account);
+    }
+}
 
 int main()
 {
+    char product[20];
+    int account;
+
+    struct LIST *first = malloc(sizeof(struct LIST));
+    struct LIST *added = NULL;
+
+    printf(">>>LINKED LIST 1<<<<<\n\n");
+    printf("Enter a new product: ");
+    scanf("%s",&product);
+    printf("Enter # of items: ");
+    scanf("%d",&account);
     
+    first = getNewItem(product,account);
+    
+    printf("\n<<<FIRST<<<<\n");
+    printData(first,"first");
+
     return 0;
 }
+
