24339a9ddf6f470db3ddfa7c730220ab40b6bb4f
Aaron Moreno Villeda <hh1360@hotmail.com>
2022-11-12 22:33:51 -0600

    Traverse and print array completed

--- BigArrayOfLinkedList.c
+++ BigArrayOfLinkedList.c
@@ -10,54 +10,105 @@ int main()
 {
     //DIMENSIONS
     int monthNum = 2;
-    int days[]={2,5};
+    int days[]={1,2};
+    int tasks[]={2,4,3};
+    int taskNum = 0;
 
-    //STATIC POINTER ARRAY 
-    test ***head = malloc(sizeof(test**)*monthNum);
+    //STATIC POINTER ARRAY (YEAR)
+    test ***head = malloc(sizeof(test**)*monthNum);//ALOCATE MEMORY FOR AS MANY POINTERS TO POINTER AS # OF MONTHS
 
-    //INITIALIZE MONTHS ARRAY
-    for(int i = 0; i < 2; i++)
+    //INITIALIZE POINTERS TO POINTER ARRAY (MONTHS)
+    for(int i = 0; i < monthNum; i++)
     {
-        head[i] = malloc(sizeof(test**[days[i]]));
+        head[i] = malloc(sizeof(test*[days[i]]));//ALOCATE MEMORY FOR THE # OF DAYS ON EACH MONTH
         
-        //SET YOUR MONTHS TO NULL
-        for(int j = 0; j < days[i]; j++)
-        {
-            head[i][j] = NULL;
-        }
+        //SET EVERY DAY TO NULL
+        for(int j = 0; j < days[i]; j++){head[i][j] = NULL;}
     }
 
-    //TRAVERSE THE POINTER ARRAY 
+    /*FIND # OF TASKS
+    while(taskNum < sizeof(tasks)/sizeof(tasks[0])){taskNum++;}
+    */
+
+    //TRAVERSE THE ARRAY
+ 
     for(int i = 0; i < monthNum; i++)
     {
         for(int j = 0; j < days[i]; j++)
         {
-            //CREATE A NEW NODE
-            test *new = calloc(1,sizeof(test));
-
-            if(new != NULL)//CHECK CORRECT MEMORY ALOCATION
+            //printf("MONTH [%d] DAY [%d]\n",i,j);
+            for(int k = 0; k < tasks[taskNum]; k++)
             {
-                 //INPUT DATA
-                new->number = 13;
-                new->next = NULL;
+                test *new = malloc(sizeof(test));
+                if(new != NULL)//CHECK CORRECT MEMORY ALOCATION
+                {
+                    new->number = 13;
+                    new->next = NULL;
 
-                if(head[i][j] == NULL) {head[i][j] = new;}//CREATE NEW LIST IF THE DAY IS EMPTY
+                    if(head[i][j] == NULL){head[i][j] = new;}//CREATE NEW LIST IF THE DAY IS EMPTY
+                    else //ADD AT THE TOP OF THE LIST
+                    {
+                        new->next = head[i][j];
+                        head[i][j] = new;
+                    }
+                    //printf("NUMBER ALOCATED\n");
+                }
                 else
                 {
-                    new->next = head[i][j];
-                    head[i][j] = new;
-                }//ADD AT THE TOP OF THE LIST
+                    printf("\n****ALOCATION FAILURE****\n");
+                    return 1;
+                }
+                new = NULL;
+                free(new);
             }
-            else
+            taskNum++;
+            printf("\n");
+        }
+    }
+
+    //PRINT ALL DAYS
+    for(int i = 0; i < monthNum; i++)
+    {
+        for(int j = 0; j < days[i]; j++)
+        {
+            printf("MONTH [%d] DAY [%d]\n",i,j);           
+            test *temp = head[i][j];
+            while(temp != NULL)
             {
-                printf("***ERROR***");
-                return 1;
+                printf("%d\t",temp->number);
+                temp = temp->next;
+            }
+            temp = NULL;
+            free(temp);
+            printf("\n");
+        }
+    }
+    //TODO: check if memory is cleaned correctly
+
+
+    //CLEAN UP MEMORY
+    for(int i = 0; i < monthNum; i++)
+    {
+        for(int j = 0; j < days[i]; j++)
+        {
+            int cont = 1;
+            test *temp = head[i][j];
+            test *next;
+            printf("\nCLEANING [%d][%d]\n",i,j);
+
+            while(temp)
+            {
+                next = temp->next;
+                free(temp);
+                temp = next;
+                printf("task: %d\n",cont);
+                cont++;
             }
             
         }
     }
 
-    printf("***PROGRAM FINISHED***");
+    printf("\n***PROGRAM FINISHED***\n");
     return 0;
 }
 
